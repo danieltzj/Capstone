ab71ed40322e6e31bcb50ac696bfe9e4dc1ab9c1
Daniel Tan <daniel_tzj@hotmail.com>
2018-07-16 23:19:10 -0700

    Added button logic

--- Application.py
+++ Application.py
@@ -13,7 +13,7 @@ import pyocr.builders
 
 def convert(character):
     # space character check, turn off all actuators
-    if character == 'a':
+    if character == ' ':
         piezo1.off()
         piezo2.off()
         piezo3.off()
@@ -340,7 +340,7 @@ def convert(character):
         piezo4.off()
         piezo5.off()
         piezo6.on()
-   elif character == ')':
+    elif character == ')':
         piezo1.off()
         piezo2.off()
         piezo3.off()
@@ -366,13 +366,13 @@ def convert(character):
 
         sleep(1)
 
-        piezo1.off))
+        piezo1.off()
         piezo2.off()
         piezo3.on()
         piezo4.on()
         piezo5.off()
         piezo6.off()
-    elif character == '\':
+    elif character == '\\':
         piezo1.off()
         piezo2.off()
         piezo3.off()
@@ -389,8 +389,10 @@ def convert(character):
         piezo5.off()
         piezo6.on()
 
+
 # initialize camera module
 camera = PiCamera()
+txt = 0
 
 # intialize pyocr
 tools = pyocr.get_available_tools()
@@ -407,39 +409,79 @@ print("Available languages: %s" % ", ".join(langs))
 lang = langs[0]
 print("Will use lang '%s'" % (lang))
 
-#camera.start_preview()
-#sleep(10)
-#camera.capture('/home/pi/Capstone/test.png')       
-#camera.stop_preview()
-
-
-#txt = tool.image_to_string(
-#    Image.open('/home/pi/Capstone/test.png'),
-#    lang="eng",
-#    builder=pyocr.builders.TextBuilder()
-#)
-#print(txt)
-# txt is a Python string
+# Intialize GPIO
+captureButton = Button(2)
+playPauseButton = Button(3)
+backButton = Button(4)
 
 # Piezoeletric actuator layout corresponding to numbered piezo variables
 # 1 4
 # 2 5
 # 3 6
-piezo1 = OutputDevice(17)
-piezo2 = OutputDevice(18)
-piezo3 = OutputDevice(27)
-piezo4 = OutputDevice(22)
-piezo5 = OutputDevice(23)
-piezo6 = OutputDevice(24)
+piezo1 = OutputDevice(26)
+piezo2 = OutputDevice(19)
+piezo3 = OutputDevice(13)
+piezo4 = OutputDevice(6)
+piezo5 = OutputDevice(5)
+piezo6 = OutputDevice(12)
 
-buzz = Buzzer(16)
-sensor = DistanceSensor(23,24)
+# Buzzer and sensor
+bz = Buzzer(16)
+sensor = DistanceSensor(20,21)
 
-while True:
-    if sensor.distance < 0.23 and sensor.distance > 0.26:
-        bz.off()
-    else:
-        bz.on()
+# turn off the buzzer first
+bz.on()
 
+def capture_and_convert():
+    camera.capture('/home/pi/Capstone/test.png')
 
+    txt = tool.image_to_string(
+        Image.open('/home/pi/Capstone/test.png'),
+        lang="eng",
+        builder=pyocr.builders.TextBuilder()
+    )
+    #print(txt)
+    # txt is a Python string
+    
 
+# Main Program Loop
+while(True):
+    # wait for the capture button to be pressed to start the sensor
+    # Buzz when the text is in the distance threshold of the camera
+    captureButton.wait_for_press()
+    while(True):
+        print(sensor.distance)
+        if sensor.distance > 0.22 and sensor.distance < 0.24:
+            bz.beep()
+            captureButton.when_pressed = capture_and_convert
+
+    # Start outputting Braille
+    i = 0 # counter for which character
+    while( i < len(txt)):
+        convert(txt[i])
+        sleep(0.4)
+         
+            if playPauseButton.is_pressed:
+                continue
+            else if backButton.is_pressed:
+                i--
+
+while(True):
+    piezo1.on()
+    piezo2.on()
+    piezo3.on()
+    piezo4.on()
+    piezo5.on()
+    piezo6.on()
+    sleep(2)
+    piezo1.off()
+    piezo2.off()
+    piezo3.off()
+    piezo4.off()
+    piezo5.off()
+    piezo6.off()
+    sleep(2)
+
+#camera.start_preview()
+#sleep(10)
+#camera.stop_preview()
